8bf498c3e89a7820958e46679ed80cf35a45b4b2
Dhrupal Shah <dhrupal@sellergize.com>
2025-10-19 18:15:22 +0100

    Initial Setup

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+// Import PrimeVue components
+import Button from 'primevue/button';
+import Card from 'primevue/card';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
+            >
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="p-6">
+                    <Card>
+                        <template #title>Git-Monitor Project</template>
+                        <template #content>
+                            <p class="m-0">
+                                Welcome! Your project is successfully set up
+                                with Laravel, Vue, TypeScript, and PrimeVue.
+                            </p>
+                        </template>
+                        <template #footer>
+                            <Button icon="pi pi-check" label="Awesome!" />
+                        </template>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ resources/js/app.ts
@@ -0,0 +1,38 @@
+import '../css/app.css';
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, DefineComponent, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+// PrimeVue and Theme imports
+import Aura from '@primevue/themes/aura'; // Import the new Aura theme
+import 'primeicons/primeicons.css'; // Icons
+import PrimeVue from 'primevue/config';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(PrimeVue, {
+                ripple: true,
+                theme: {
+                    preset: Aura, // Set Aura as the theme preset
+                },
+            })
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
